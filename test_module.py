182a85289b898e88d7b1e1aa3c0aa448fbaeed60
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-05-25 00:37:38 +0300

    TST: add some more tests

--- test_module.py
+++ test_module.py
@@ -8,6 +8,21 @@ def func():
 
 def func2():
     """
+    Check that `np.` is imported and the array repr is recognized
     >>> np.array([1, 2, 3.0])
     array([1, 2, 3])
+
+    Check that the comparison is with atol and rtol: give less digits than
+    what numpy by default prints  
+    >>> np.sin([1., 2, 3])
+    array([0.8414, 0.9092, 0.1411])
     """
+
+def func3():
+    """
+    Check that printed arrays are checked with atol/rtol
+    >>> a = np.array([1, 2, 3, 4]) / 3
+    >>> print(a)
+    [0.33  0.66  1  1.33]
+    """
+
