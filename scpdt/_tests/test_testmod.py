973e7497d98409a59c8cb81f35c530f81068de19
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-05-28 18:02:11 +0300

    TST: split individual test modules

---
+++ scpdt/_tests/test_testmod.py
@@ -0,0 +1,32 @@
+from . import module, stopwords
+from .._run import testmod
+
+_VERBOSE = True
+
+
+def test():
+    test_module()
+    test_module_vanilla_dtfinder()
+    test_stopwords()
+
+
+def test_module():
+    res = testmod(module, verbose=_VERBOSE)
+    if res.failed != 0 or res.attempted == 0:
+        raise RuntimeError("Test_module(DTFinder) failed)")
+    return res
+
+
+def test_module_vanilla_dtfinder():
+    res = testmod(module, verbose=_VERBOSE, use_dtfinder=False)
+    if res.failed != 0 or res.attempted == 0:
+        raise RuntimeError("Test_module(vanilla DocTestFinder) failed)")
+    return res
+
+
+def test_stopwords():
+    res = testmod(stopwords, verbose=_VERBOSE)
+    if res.failed != 0 or res.attempted == 0:
+        raise RuntimeError("Test_module(vanilla DocTestFinder) failed)")
+    return res
+
