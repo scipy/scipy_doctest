6b6ca1df5b70435c3ecc7a40493e23cdcd732e7f
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-05-29 12:57:12 +0300

    MAINT: port get_all_dict from refguide-check

--- scpdt/_util.py
+++ scpdt/_util.py
@@ -4,6 +4,7 @@ Assorted utilities.
 import os
 import warnings
 import shutil
+import copy
 import tempfile
 from contextlib import contextmanager
 
@@ -42,3 +43,56 @@ def temp_cwd():
     finally:
         os.chdir(cwd)
         shutil.rmtree(tmpdir)
+
+
+def is_deprecated(f):
+    """ Check if an item is deprecated.
+    """
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("error")
+        try:
+            f(**{"not a kwarg":None})
+        except DeprecationWarning:
+            return True
+        except Exception:
+            pass
+        return False
+
+
+def get_all_list(module):
+    """Return a copy of the __all__ list with irrelevant items removed.
+
+    - If __all__ is missing, process the output of `dir(module)`.
+    - Also return a list of deprecated items and "other" items, which failed
+      to classify.
+    """
+    if hasattr(module, "__all__"):
+        all_list = copy.deepcopy(module.__all__)
+    else:
+        all_list = copy.deepcopy(dir(module))
+        all_list = [name for name in all_list
+                    if not name.startswith("_")]
+    for name in ['absolute_import', 'division', 'print_function']:
+        try:
+            all_list.remove(name)
+        except ValueError:
+            pass
+
+    # Modules are almost always private; real submodules need a separate
+    # run of refguide_check.
+    all_list = [name for name in all_list
+                if not inspect.ismodule(getattr(module, name, None))]
+
+    deprecated = []
+    not_deprecated = []
+    for name in all_list:
+        f = getattr(module, name, None)
+        if callable(f) and is_deprecated(f):
+            deprecated.append(name)
+        else:
+            not_deprecated.append(name)
+
+    others = set(dir(module)).difference(set(deprecated)).difference(set(not_deprecated))
+
+    return not_deprecated, deprecated, others
+
