28e0790d001713f711aefc026033a56249c365a8
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-06-05 23:22:53 +0300

    MAINT: add run_doctest_examples to __init__.py

--- scpdt/__init__.py
+++ scpdt/__init__.py
@@ -8,7 +8,7 @@ Whitespace-insensitive, numpy-aware, floating-point-aware doctest helpers.
 __version__ = "0.1"
 
 from ._impl import DTChecker, DTFinder, DTRunner, DebugDTRunner, DTConfig
-from ._frontend import testmod, find_doctests
+from ._frontend import testmod, find_doctests, run_docstring_examples
 
 from ._tests import test
 
