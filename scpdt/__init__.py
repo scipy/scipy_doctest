0e4d15cb02a741f6fb9b1e1acfde7375ba312047
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-06-06 00:28:52 +0300

    ENH: add a basic CLI for *.py files
    Can do `$ python -m scpdt pythonsource.py`' to mimic `$ python -m doctest`

--- scpdt/__init__.py
+++ scpdt/__init__.py
@@ -10,5 +10,3 @@ __version__ = "0.1"
 from ._impl import DTChecker, DTFinder, DTRunner, DebugDTRunner, DTConfig
 from ._frontend import testmod, find_doctests, run_docstring_examples
 
-from ._tests import test
-
---
+++ scpdt/__main__.py
@@ -0,0 +1,6 @@
+import sys
+
+from ._frontend import _main
+
+if __name__ == "__main__":
+    sys.exit(_main())
