636a218957f0d53d200fa51796a32a262e2a5e9e
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-06-09 23:35:18 +0300

    MAINT: add testfile to __init__

--- scpdt/__init__.py
+++ scpdt/__init__.py
@@ -8,5 +8,5 @@ Whitespace-insensitive, numpy-aware, floating-point-aware doctest helpers.
 __version__ = "0.1"
 
 from ._impl import DTChecker, DTFinder, DTParser, DTRunner, DebugDTRunner, DTConfig
-from ._frontend import testmod, find_doctests, run_docstring_examples
+from ._frontend import testmod, testfile, find_doctests, run_docstring_examples
 
