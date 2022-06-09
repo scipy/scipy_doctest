af8a094074862178c4f21f496cd4816350e84ba2
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-06-09 19:04:11 +0300

    TST: add a test for parser/stopword checking

--- scpdt/_tests/test_parser.py
+++ scpdt/_tests/test_parser.py
@@ -1,4 +1,7 @@
-from .._impl import DTConfig, DTParser
+import doctest
+import pytest
+
+from .._impl import DTConfig, DTParser, DebugDTRunner
 
 def test_parser_default_config():
     # Test that parser adds the _ignore marker for stopwords
@@ -10,7 +13,6 @@ def test_parser_default_config():
     assert len(examples) == 1
     assert examples[0].source == "1 + plt.xlim([1, 2])\n"
     assert examples[0].want == "  # _ignore\n"
-
 
 def test_parser_vanilla_config():
     # Test that no stopwords means no markers
@@ -30,3 +32,30 @@ def test_config_nocopy():
     config = DTConfig()
     parser = DTParser(config)
     assert parser.config is config
+
+
+def test_stopwords_invalid_python():
+    # check that with ignore markers examples are still checked to be valid
+    # python. This string raises
+    # TypeError("unsupported operand type(s) for +: 'int' and 'tuple'")
+
+    string = ">>> import matplotlib.pyplot as plt; 1 + plt.xlim([1, 2])\n"
+
+    config = DTConfig()
+    parser = DTParser(config)
+
+    test = parser.get_doctest(string,
+                               globs=config.default_namespace,
+                               name='none',
+                               filename='none',
+                               lineno=0)
+    runner = DebugDTRunner()
+    with pytest.raises(doctest.UnexpectedException) as exc_info:
+        runner.run(test)
+
+    orig_error = exc_info.value.exc_info  # unwrap pytest -> doctest -> example
+    kind, value, traceback = orig_error
+
+    assert kind is TypeError
+    assert "unsupported operand type(s)" in value.args[0]
+
