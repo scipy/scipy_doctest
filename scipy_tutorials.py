b83aac11515b7e82c5a6a65fc318ef1328a83772
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-06-10 22:38:39 +0300

    ENH: add a scipy-specific pseudocode marker

--- scipy_tutorials.py
+++ scipy_tutorials.py
@@ -1,15 +1,26 @@
 import os
 import glob
 
-from scpdt import testfile
+from scpdt import testfile, DTConfig
 
 base_dir = "/home/br/repos/scipy/scipy/doc/source/tutorial/"
 tut_path = os.path.join(base_dir, '*.rst')
 print('\nChecking tutorial files at %s:' % os.path.relpath(tut_path, os.getcwd()))
 
-for filename in sorted(glob.glob(tut_path)):
-  #  print(filename)
-  #  print("="*len(filename))
+tutorials = [f for f in sorted(glob.glob(tut_path))]
 
-    testfile(filename, module_relative=False, verbose=1, raise_on_error=False, report=True)
+# XXX: remove
+tutorials = [f for f in tutorials if 'io.rst' not in f]
+#tutorials = [f for f in tutorials if 'integrate' in f]
+
+### set up scipy-specific config
+config = DTConfig(pseudocode=set(['integrate.nquad(func,']))
+
+
+for filename in tutorials:
+    print(filename)
+    print("="*len(filename))
+
+    testfile(filename, module_relative=False,
+             verbose=0, raise_on_error=False, report=True, config=config)
 
