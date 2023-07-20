c991c185148b44b51d4104240eecf34ab2817d9e
Sheila-nk <sheilankw@gmail.com>
2023-07-20 16:55:08 +0300

    add all tests for array_abbreviation

--- scpdt/tests/test_pytest_configuration.py
+++ scpdt/tests/test_pytest_configuration.py
@@ -1,6 +1,6 @@
 import pytest
-
 pytest_plugins = ['pytester']
+
 
 def test_array_abbreviation(pytester):
     """
@@ -10,7 +10,9 @@ def test_array_abbreviation(pytester):
     file_content = """
     def array_abbreviation():
         '''
-        >>> import numpy as np    
+        >>> np.arange(10000)
+        array([0, 1, 2, ..., 9997, 9998, 9999])
+
         >>> np.diag(np.arange(33)) / 30
         array([[0., 0., 0., ..., 0., 0.,0.],
             [0., 0.03333333, 0., ..., 0., 0., 0.],
@@ -19,11 +21,19 @@ def test_array_abbreviation(pytester):
             [0., 0., 0., ..., 1., 0., 0.],
             [0., 0., 0., ..., 0., 1.03333333, 0.],
             [0., 0., 0., ..., 0., 0., 1.06666667]])
+
+
+        >>> np.diag(np.arange(1, 1001, dtype=float))
+        array([[1,    0,    0, ...,    0,    0,    0],
+            [0,    2,    0, ...,    0,    0,    0],
+            [0,    0,    3, ...,    0,    0,    0],
+                ...,
+            [0,    0,    0, ...,  998,    0,    0],
+            [0,    0,    0, ...,    0,  999,    0],
+            [0,    0,    0, ...,    0,    0, 1000]])
         '''
         pass
         """
     pytester.makepyfile(file_content)
-
-    result = pytester.runpytest("--doctest-modules")
-    
+    result = pytester.runpytest("--doctest-modules") 
     assert result.ret == pytest.ExitCode.OK
