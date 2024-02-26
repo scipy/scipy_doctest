0bed0d4c7a207ac3083170084d3b4d0eb16af174
Sheila-nk <sheilankw@gmail.com>
2024-02-26 15:13:08 +0300

    change local file path

--- scpdt/tests/local_file_cases.py
+++ scpdt/tests/local_file_cases.py
@@ -3,9 +3,9 @@ from scpdt.conftest import dt_config
 
 # Specify local files required by doctests
 dt_config.local_resources = {'scpdt.tests.local_file_cases.local_files':
-                                  ['scpdt/tests/local_file.txt'],
+                                  ['local_file.txt'],
                             'scpdt.tests.local_file_cases.sio':
-                                  ['scpdt/tests/octave_a.mat']   
+                                  ['octave_a.mat']   
                                   }
 
 __all__ = ['local_files', 'sio']
