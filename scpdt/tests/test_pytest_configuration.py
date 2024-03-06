1ba20a080ff53bfa8d823769e7b896253b34bb1a
Sheila-nk <sheilankw@gmail.com>
2024-03-06 11:53:29 +0300

    add workflow for tests that don't require scipy and MPL

--- scpdt/tests/test_pytest_configuration.py
+++ scpdt/tests/test_pytest_configuration.py
@@ -1,8 +1,7 @@
 import pytest
-from pathlib import PosixPath
+from pathlib import Path
 
 from . import module_cases, failure_cases, failure_cases_2, stopwords_cases, local_file_cases
-
 
 # XXX: this is a bit hacky and repetetive. Can rework?
 
@@ -27,7 +26,7 @@ except Exception:
 def test_module_cases(pytester):
     """Test that pytest uses the DTChecker for doctests."""
     path_str = module_cases.__file__
-    python_file = PosixPath(path_str)
+    python_file = Path(path_str)
     result = pytester.inline_run(python_file, "--doctest-modules")
     assert result.ret == pytest.ExitCode.OK
 
@@ -36,16 +35,16 @@ def test_failure_cases(pytester):
     file_list = [failure_cases, failure_cases_2]
     for file in file_list:
         path_str = file.__file__
-        python_file = PosixPath(path_str)
+        python_file = Path(path_str)
         result = pytester.inline_run(python_file, "--doctest-modules")
     assert result.ret == pytest.ExitCode.TESTS_FAILED
     
 
-@pytest.mark.skipif(not HAVE_MATPLOTLIB, reason='need matplotlib')
+@pytest.mark.skipif(not HAVE_SCIPY, reason='need scipy')
 def test_stopword_cases(pytester):
     """Test that pytest uses the DTParser for doctests."""
     path_str = stopwords_cases.__file__
-    python_file = PosixPath(path_str)
+    python_file = Path(path_str)
     result = pytester.inline_run(python_file, "--doctest-modules")
     assert result.ret == pytest.ExitCode.OK
 
@@ -55,6 +54,6 @@ def test_local_file_cases(pytester):
     """Test that local files are found for use in doctests.
     """
     path_str = local_file_cases.__file__
-    python_file = PosixPath(path_str)
+    python_file = Path(path_str)
     result = pytester.inline_run(python_file, "--doctest-modules")
     assert result.ret == pytest.ExitCode.OK
