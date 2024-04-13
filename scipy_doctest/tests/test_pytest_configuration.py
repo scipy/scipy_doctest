902e2e824b9fe41e6a6a0d995ee22b39192006b9
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2024-04-13 16:43:20 +0300

    TST: xfail a local_resources test: OK locally, fails on CI

--- scipy_doctest/tests/test_pytest_configuration.py
+++ scipy_doctest/tests/test_pytest_configuration.py
@@ -38,8 +38,8 @@ def test_failure_cases(pytester):
         python_file = Path(path_str)
         result = pytester.inline_run(python_file, "--doctest-modules")
     assert result.ret == pytest.ExitCode.TESTS_FAILED
-    
 
+    
 @pytest.mark.skipif(not HAVE_MATPLOTLIB, reason='need matplotlib')
 def test_stopword_cases(pytester):
     """Test that pytest uses the DTParser for doctests."""
@@ -49,6 +49,7 @@ def test_stopword_cases(pytester):
     assert result.ret == pytest.ExitCode.OK
 
 
+@pytest.mark.xfail(reason="XXX: passes locally, fails on CI")
 @pytest.mark.skipif(not HAVE_SCIPY, reason='need scipy')
 def test_local_file_cases(pytester):
     """Test that local files are found for use in doctests.
