9f3602458c4b5e24dc1fd25259da1cc6dcf79ead
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2024-06-08 19:55:33 +0300

    REL: bump version to 1.2.0

--- scipy_doctest/__init__.py
+++ scipy_doctest/__init__.py
@@ -5,7 +5,7 @@ Whitespace-insensitive, numpy-aware, floating-point-aware doctest helpers.
 """
 
 
-__version__ = "1.2dev0"
+__version__ = "1.2.0"
 
 from .impl import DTChecker, DTFinder, DTParser, DTRunner, DebugDTRunner, DTConfig  # noqa
 from .frontend import testmod, testfile, find_doctests, run_docstring_examples      # noqa
