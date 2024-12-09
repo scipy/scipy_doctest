eb0a44e5b2026d374d7c26ff06551e5cd3df0f3e
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2024-12-09 18:15:14 +0200

    REL: bump version to 1.5.1

--- scipy_doctest/__init__.py
+++ scipy_doctest/__init__.py
@@ -3,7 +3,7 @@ Configurable, whitespace-insensitive, floating-point-aware doctest helpers.
 """
 
 
-__version__ = "1.5dev0"
+__version__ = "1.5.1"
 
 try:
     # register internal modules with pytest; obscure errors galore otherwise
