f32439c40853e271d9b32dac5bbba2aebcb23297
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2025-08-08 10:28:13 +0200

    REL: bump version to 2.0.1

--- scipy_doctest/__init__.py
+++ scipy_doctest/__init__.py
@@ -3,7 +3,7 @@ Configurable, whitespace-insensitive, floating-point-aware doctest helpers.
 """
 
 
-__version__ = "2.0.1.dev0"
+__version__ = "2.0.1"
 
 try:
     # register internal modules with pytest; obscure errors galore otherwise
